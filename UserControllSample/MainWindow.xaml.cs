80a1bed393fd329b6ebcb00dcaf1095d4e1494d4
fullname <test@test.com>
2017-07-31 05:41:15 +0900

    UserControll Bind Sample Relativesource

---
+++ UserControllSample/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Reactive.Bindings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserControllSample
+{
+    public class MainWindowViewModel
+    {
+        public ReactiveProperty<string> lab { get; private set; } = new ReactiveProperty<string>("tes");
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindowViewModel VM = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            VM = new MainWindowViewModel();
+            DataContext = VM;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            VM.lab.Value += "!";
+        }
+    }
+}
---
+++ UserControllSample/UserControl1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserControllSample
+{
+    /// <summary>
+    /// UserControl1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public static readonly DependencyProperty LabelProperty
+            = DependencyProperty.Register("Label", typeof(string), typeof(UserControl1));//, new UIPropertyMetadata("Label"));
+
+        [BindableAttribute(true)]
+        public string Label
+        {
+            get { return (string)GetValue(LabelProperty); }
+            set { SetValue(LabelProperty, value); }
+        }
+        public UserControl1()
+        {
+            InitializeComponent();
+            Label = "testest";
+        }
+    }
+}
