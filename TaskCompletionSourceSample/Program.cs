0aac4de0d7db18883f17a08e3f5a33d49f17c146
fullname <test@test.com>
2017-12-29 11:25:16 +0900

    taskためした

---
+++ TaskCompletionSourceSample/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskCompletionSourceSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskCompletionSource<int> tcs = new TaskCompletionSource<int>();
+            var task = tcs.Task;
+
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                Thread.Sleep(1000);
+                tcs.SetResult(123);
+            });
+
+            Console.WriteLine("waiting...");
+            // waitはなくてもResultに値が入るまで待ってくれる
+            task.Wait();
+            Console.WriteLine("running...");
+            Console.WriteLine($"task computed {task.Result}");
+        }
+    }
+}
